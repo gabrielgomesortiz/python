6dbff219cf05a5a17bb6a346ecbe70694d28ee29
gabrielgomesortiz <gabrielgomesortiz@gmail.com>
2026-01-21 14:11:54 -0300

    feito e terminado o algoritmo de ordenação quick sort

--- aplicando/metodos_de_odenacao/quick_sort.py
+++ aplicando/metodos_de_odenacao/quick_sort.py
@@ -3,9 +3,32 @@ inteiros em ordem crescente, utilizando a técnica de divisão e conquista. A im
 forma recursiva, realizando a partição da lista a partir de um elemento pivô e reorganizando os elementos
 menores à esquerda e os maiores à direita. Ao final da execução, a lista original deve estar ordenada.'''
 
-lista = [5, 3, 8, 4, 2]
 
+def controle_fluxo():
+    lista = [5, 3,1,1,2,3,4,0.5,999999, 8,5,5,5, 4, 2, 80,1.5,987,3,0.5]
+    ordenado = quick_sort(lista)
+    return ordenado
+
+#aqui é o coração do algoritmo. aqui fiz 3 partições da lista: > , < e =
 def quick_sort(lista):
-    
-    return 0
-print(f"como isso é legal {quick_sort(lista)}")
+    length = len(lista)
+    if length <= 1:
+        return lista
+    pivo = lista[0]
+    maiores =[]
+    iguais = [pivo]
+    menores = []
+    i = 1
+    while i < length:
+
+        if lista[i] < pivo:
+            menores.append(lista[i])
+        elif lista[i] > pivo:
+            maiores.append(lista[i])
+        else:
+            iguais.append(lista[i])
+        i+=1
+
+    return quick_sort(menores) + iguais + quick_sort(maiores)
+
+print(controle_fluxo())
