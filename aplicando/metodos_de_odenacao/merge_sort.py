7fd8cac8444e1e969ccf7cff82843899fb3b17a4
gabrielgomesortiz <gabrielgomesortiz@gmail.com>
2026-01-22 22:20:24 -0300

    Terminado o merge sort

--- aplicando/metodos_de_odenacao/merge_sort.py
+++ aplicando/metodos_de_odenacao/merge_sort.py
@@ -6,12 +6,12 @@ Ao final da execução, a lista original deve estar ordenada em ordem crescente.
 
 
 def controle_fluxo():    
-    lista = [5, 3,1,3,6,9,0]
+    lista = [5, 3,1,3,6,9,0,98,2,4,5,7,8,2,323,1,4,6,0,7]
     ordenado = dividir_array(lista)
     return ordenado
 
 def dividir_array(lista):
-    if len(lista)<=1:   
+    if len(lista) <= 1:
         return lista
     
     meio = len(lista)//2
@@ -24,9 +24,28 @@ def dividir_array(lista):
         else:
             direita.append(lista[i])
         i+= 1
-    return dividir_array(esquerda), dividir_array(direita)
+    esquerda = dividir_array(esquerda)
+    direita = dividir_array(direita)
 
-    
+    return merge(esquerda,direita)
 
+
+def merge(esquerda,direita):
+    resultado = []
+    i = j= 0
+    while i < len(esquerda) and j < len(direita):
+        if esquerda[i] >= direita[j]:
+            resultado.append(direita[j])
+            j+=1
+        else:
+            resultado.append(esquerda[i])
+            i+= 1
+    while i < len(esquerda):
+        resultado.append(esquerda[i])
+        i+= 1
+    while j < len(direita):
+        resultado.append(direita[j])
+        j+= 1
+    return resultado
 
 print(controle_fluxo())
